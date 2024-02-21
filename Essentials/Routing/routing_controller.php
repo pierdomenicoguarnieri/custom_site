9aa7923a116aec9e2a040f9140471ccaab9037b4
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2024-02-21 22:22:57 +0100

    added routing_controller

---
+++ Essentials/Routing/routing_controller.php
@@ -0,0 +1,23 @@
+<?php
+    if(!empty($_COOKIE['acode'])){
+        $path = ADMINPATH;
+    }elseif(!empty($_COOKIE['ucode'])){
+        $path = USERPATH;
+    }else{
+        $path = GLOBALPATH;
+    }
+    
+    $files_views = scandir($path."views");
+    $flag = false;
+    $page = isset($_GET['page']) ? $_GET['page'] : 'index';
+    if($page != '.' && $page != '..' && strlen($page) > 0){
+        if(in_array($page.'.php',$files_views)){
+            require_once $path.'views/'.$file.'.php';
+            $flag = true;
+        }
+    }
+
+    if(!$flag){ ?>
+        <script>window.location.href="<?php echo DOMAIN.'404.php' ?>"</script>
+    <?php }
+?>
